3814d4c9afb4949d789734eb3ff2dbac2619e1e2
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2021-03-23 13:20:57 -0300

    Product

--- src/main/java/br/com/tiagosouzagarcia/course/config/TestConfig.java
+++ src/main/java/br/com/tiagosouzagarcia/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.tiagosouzagarcia.course.entities.Category;
 import br.com.tiagosouzagarcia.course.entities.Order;
+import br.com.tiagosouzagarcia.course.entities.Product;
 import br.com.tiagosouzagarcia.course.entities.User;
 import br.com.tiagosouzagarcia.course.entities.enums.OrderStatus;
 import br.com.tiagosouzagarcia.course.repositories.CategoryRepository;
 import br.com.tiagosouzagarcia.course.repositories.OrderRepository;
+import br.com.tiagosouzagarcia.course.repositories.ProductRepository;
 import br.com.tiagosouzagarcia.course.repositories.UserRepository;
 
 @Configuration
@@ -28,9 +30,22 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ProductRepository productRepositury;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -39,12 +54,8 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
 		
-		Category cat1 = new Category(null, "Electronics");
-		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Computers");
-		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
+		productRepositury.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
---
+++ src/main/java/br/com/tiagosouzagarcia/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package br.com.tiagosouzagarcia.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.tiagosouzagarcia.course.entities.Product;
+import br.com.tiagosouzagarcia.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
